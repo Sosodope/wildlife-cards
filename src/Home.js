b654a012a505d64d64a3d9198929e130263c1480
Sosodope <sonadolkar93@hotmail.com>
2018-09-16 23:12:57 +0700

    add species list and card for each one

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,25 @@
 import React, { Component } from "react";
 import Card from "./components/Card";
 import "./style.css";
+import { wildlifes } from "./wildlifes";
 
 class Home extends Component {
   render() {
     return (
       <div className="container">
-        <h1>Home</h1>
-        <div className="content">
-          <Card />
-          <Card />
-          <Card />
+        <h1>Endangered Species list</h1>
+        <div className="wildlife">
+          {wildlifes.map(wildlife => {
+            return (
+              <Card
+                key={wildlife.id}
+                img={wildlife.img}
+                name={wildlife.name}
+                scientificName={wildlife.scientificName}
+                wildPopulation={wildlife.wildPopulation}
+              />
+            );
+          })}
         </div>
       </div>
     );
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,19 @@
 import React from "react";
 
-const Card = () => {
+const Card = props => {
+  const { id, img, name, scientificName, wildPopulation } = props;
   return (
-    <div className="card">
-      <img
-        src="https://images.unsplash.com/photo-1496316778157-e826f5bb47f2?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=f221991eeab09a9b12c98afdce2f1199&auto=format&fit=crop&w=1955&q=80"
-        alt=""
-        className="card-image"
-      />
+    <div className="tc bg-gold dib br3 pa3 ma2 grow bw2 shadow-5">
+      <img src={img} alt={name} className="card-image" />
       <div>
-        <h2>Rhino</h2>
-        <p>Africa, Asia</p>
+        <h2>{name} </h2>
+        <p>{scientificName} </p>
+        <p>
+          <span className="pop-icon">
+            <i class="fas fa-dove" />
+          </span>
+          {wildPopulation}
+        </p>
       </div>
     </div>
   );
